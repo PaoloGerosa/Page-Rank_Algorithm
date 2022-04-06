a02014199982cad8f75070efe8381231c410dfaa
PaoloGerosa <64699038+PaoloGerosa@users.noreply.github.com>
2022-04-06 10:16:14 +0200

    Delete Credentials.py

--- Credentials.py
+++
@@ -1,6 +0,0 @@
-# Twitter App Credentials (Reserarch account)
-consumer_key = "tvZH2VyHusWSD8G36JWVphAVe"
-consumer_secret = "1K6Vx0GgMRnlPtktjUYZyvk0fWFbYyPCicSLHayHE215odI3Pv"
-
-access_token = "948252799581806593-dSnFViQ5DhNW9PHk5HGaTj6ZzrcakTm"
-access_token_secret = "pSe4H3JLaEa5RHr2fdvu1j8LUpP7l1UKuXAJvGGDjH2ek"
